8b97cec5f0e0b7ab25dfa3fb6e1e889fc5186bd2
Вячеслав-Миличенко <straga78@gmail.com>
2025-04-11 21:49:30 +0300

    Fix imports after moving bot

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from telegram.ext import ApplicationBuilder
 from config import BOT_TOKEN
-from handlers.start import start_handler
-from handlers.voice import voice_handler
-from handlers.list_notes import list_handler
-from handlers.callback import callback_handler
+from start import start_handler
+from voice import voice_handler
+from list_notes import list_handler
+from callback import callback_handler
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(start_handler)
